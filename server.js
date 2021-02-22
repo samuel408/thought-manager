7a099c04fa8f44434d2c8d2364044a4a478de64f
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-02-21 16:31:21 -0800

    added port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { notes } = require('./db/db.json')
 // const fs = require('fs');
 // const path = require('path');
 const express =  require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // // parse incoming string or array data
@@ -30,6 +31,6 @@ function filterByQuery(query, notesArray) {
   });
 
 
-app.listen(3002, () => {
-    console.log(`API server now on port 3002!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
